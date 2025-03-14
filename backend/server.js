c09fd53b5f857a496b1eddaadc32d53eab32164a
adambpp <adambpp@gmail.com>
2025-03-14 12:56:13 -0600

    Added basic stuff to server.js

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+const channelsRoutes = require("./routes/channels");
+
+const app = express();
+
+// Parse JSON bodies
+app.use(express.json());
+
+// Mount route
+app.use("/api/channels", channelsRoutes);
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
